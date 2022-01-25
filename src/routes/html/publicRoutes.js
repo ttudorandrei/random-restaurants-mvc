2ac37b0d95043a3292e71eb4fa7b652ccc0b4ef3
Tudor Tocan <tudor.tocan@gmail.com>
2022-01-25 20:45:25 +0000

    render homepage with navbar

--- src/routes/html/publicRoutes.js
+++ src/routes/html/publicRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 router.get("/", (req, res) => {
   try {
-    res.json({ data: "You have hit the home page" });
+    res.render("homepage");
   } catch (error) {
     res.json({ error: "Oops! Can't load the home page" });
   }
