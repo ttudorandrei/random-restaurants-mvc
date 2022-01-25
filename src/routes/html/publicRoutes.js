5fa917679d088a90bc82594fd531b266c139767e
Tudor Tocan <tudor.tocan@gmail.com>
2022-01-25 20:32:29 +0000

    add restaurants route

--- src/routes/html/publicRoutes.js
+++ src/routes/html/publicRoutes.js
@@ -8,4 +8,12 @@ router.get("/", (req, res) => {
   }
 });
 
+router.get("/restaurants", (req, res) => {
+  try {
+    res.json({ data: "You have hit the restaurants page" });
+  } catch (error) {
+    res.json({ error: "Oops! Can't load the restaurants page" });
+  }
+});
+
 module.exports = router;
