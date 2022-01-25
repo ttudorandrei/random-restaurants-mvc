23fbeea3acb49497cf4b57e18ea044a801e7ce65
Tudor Tocan <tudor.tocan@gmail.com>
2022-01-25 21:52:14 +0000

    add data from reandom-data-api to restaurants card

--- src/routes/html/publicRoutes.js
+++ src/routes/html/publicRoutes.js
@@ -1,3 +1,5 @@
+const { default: axios } = require("axios");
+
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
@@ -9,9 +11,23 @@ router.get("/", (req, res) => {
   }
 });
 
-router.get("/restaurants", (req, res) => {
+router.get("/restaurants", async (req, res) => {
   try {
-    res.status(200).render("restaurants");
+    const getRestaurants = async () => {
+      const axiosConfig = {
+        method: "GET",
+        url: "https://random-data-api.com/api/restaurant/random_restaurant?size=5",
+      };
+
+      const response = await axios(axiosConfig);
+      const restaurants = response.data;
+
+      return restaurants;
+    };
+
+    const data = { restaurantsList: await getRestaurants() };
+
+    res.status(200).render("restaurants", { data });
   } catch (error) {
     console.error(error.message);
     res.status(500);
